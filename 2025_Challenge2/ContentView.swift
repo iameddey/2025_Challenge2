8d30c3c3ea5b5dd8563c73e23d9bf3c0f346c242
iameddey <jaeyoon7421@naver.com>
2025-04-16 11:03:32 +0900

    Initial Commit

---
+++ 2025_Challenge2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2025_Challenge2
+//
+//  Created by 김재윤 on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2025_Challenge2/_025_Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  _025_Challenge2App.swift
+//  2025_Challenge2
+//
+//  Created by 김재윤 on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct _025_Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
